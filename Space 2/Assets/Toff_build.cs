88d95f748ca3ac871c2de347aab04ef5350fc7e3
Florian Hilprecht <florian@hilprecht.de>
2018-12-31 16:45:13 +0100

    second block

---
+++ Space 2/Assets/Toff_build.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toff_build : MonoBehaviour
+{
+    public GameObject build;
+
+    // Update is called once per frame
+    void Update()
+    {
+        build.SetActive(false);
+        enabled = false;
+    }
+}
