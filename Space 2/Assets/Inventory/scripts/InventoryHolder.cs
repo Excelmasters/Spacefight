0837f546aef65c179203b0bd829b9bfab50f3246
Iceshot <florian.bartusek@gmail.com>
2019-01-18 22:16:18 +0100

    inventory

--- Space 2/Assets/Inventory/scripts/InventoryHolder.cs
+++ Space 2/Assets/Inventory/scripts/InventoryHolder.cs
@@ -6,13 +6,44 @@ public class InventoryHolder : MonoBehaviour
 {
     public int width = 2; 
     public int height = 6; //Eigenschaften der Slots wie Höhe, Breite etc.
+
     private int numSlots;
-    public ItemSlot[] slots;
+
+    private ItemSlot[] slots;
     void Awake()
     {
         numSlots = width * height;
         slots = new ItemSlot[numSlots]; // Slots werden erstellt
-        foreach (ItemSlot slot in slots)
-            Debug.Log(slot);
+        
+    }
+    private int GetPosition(int x, int y)
+    {
+        if (x < 0 || y < 0 || x >= width || y >= height)
+            return -1;
+        return y * width + x;
+    }
+
+    public ItemSlot GetSlot(int x, int y)
+    {
+        int position = GetPosition(x, y);
+
+        if (position < 0)
+        {
+            Debug.LogWarning("InventoryHolder: Invalid position!");
+            return null;
+        }
+
+            return slots[position];
+    }
+    public void SetSlot(int x, int y, ItemSlot newSlot)
+    {
+        int position = GetPosition(x, y);
+
+        if (position < 0)
+        {
+            Debug.LogWarning("InventoryHolder: Invalid position!");
+            return;
+        }
+        slots[position] = newSlot;
     }
 }
