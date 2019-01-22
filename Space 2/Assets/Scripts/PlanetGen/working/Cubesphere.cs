7b56ecd981a3f0c688ce3d841177d8cf0fd94fae
Florian Hilprecht <florian@hilprecht.de>
2019-01-22 19:48:09 +0100

    s

--- Space 2/Assets/Scripts/PlanetGen/working/Cubesphere.cs
+++ Space 2/Assets/Scripts/PlanetGen/working/Cubesphere.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 
 public class Cubesphere : MonoBehaviour
 {
-    private GameObject prefab;
-    public GameObject Planet;
+    public GameObject prefab;
     public GameObject Gen;
+    private GameObject meshObj;
     public float radius;
-    Mesh mesh;
-    [Range (0,250)]
+    [Range(0, 500)]
     public int resolution;
     public Vector3 center;
     Noise noise = new Noise();
-   public  Material medal;
+    public Material medal;
     public int size;
+    public bool doesexist = false;
 
 
 
@@ -39,8 +39,18 @@ public class Cubesphere : MonoBehaviour
 
     public GameObject Prefab { get => prefab; set => prefab = value; }
 
+
+
+
+
+
+
+
+
+
     public float Terrain(Vector3 vertice)
     {
+
         float terrainvalue = 0;
         float basefre = frequenzy;
         float depth = amplitude;
@@ -58,139 +68,157 @@ public class Cubesphere : MonoBehaviour
     }
 
 
-        private void OnValidate()
+
+
+
+
+
+
+
+
+
+
+
+
+    private void OnValidate()
     {
-        mesh.Clear();
-        if (Planet.GetComponent<MeshFilter>() == null) { Planet.AddComponent<MeshFilter>(); }
-        if (Planet.AddComponent<MeshRenderer>() == null) { Planet.AddComponent<MeshFilter>(); }
-        foreach (Transform child in Planet.transform)
+
+        Mesh mesh = new Mesh();
+        for (int f = 0; f < 6; f++)
         {
-            Destroy(child.gameObject);
-        }
-
-        mesh = new Mesh();
-        List<Vector3> vertices = new List<Vector3>();
-        List<int> triangles = new List<int>();
-        //mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
-        vertices.Clear();
-        //vertices.Add(new Vector3(0, 0, 0));
-        //vertices.Add(new Vector3(1, 0, 0));
-        // vertices.Add(new Vector3(1, 1, 0));
-        //vertices.Add(new Vector3(0, 1, 0));
+            
+            if (Gen.GetComponent<MeshFilter>() == null) { Gen.AddComponent<MeshFilter>(); }
+            if (Gen.AddComponent<MeshRenderer>() == null) { Gen.AddComponent<MeshFilter>(); }
 
 
-        for (int i = 0; i < resolution + 1; i++)
-        {
-            for (int h = 0; h < resolution + 1; h++)
+            List<Vector3> vertices = new List<Vector3>();
+            List<int> triangles = new List<int>();
+            mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+            vertices.Clear();
+            //vertices.Add(new Vector3(0, 0, 0));
+            //vertices.Add(new Vector3(1, 0, 0));
+            // vertices.Add(new Vector3(1, 1, 0));
+            //vertices.Add(new Vector3(0, 1, 0));
+
+
+            for (int i = 0; i < resolution + 1; i++)
             {
-                vertices.Add(new Vector3((float)h / resolution * size - 0.5f, (float)i / resolution * size - 0.5f, 0 - 0.5f));
+                for (int h = 0; h < resolution + 1; h++)
+                {
+                    vertices.Add(new Vector3((float)h / resolution * size - 0.5f, (float)i / resolution * size - 0.5f, 0 - 0.5f));
+                }
+
+
+
+
+            }
+            for (int i = 0; i < vertices.Count; i++)
+            {
+                vertices[i] = vertices[i].normalized;
+            }
+            for (int i = 0; i < vertices.Count; i++)
+            {
+                vertices[i] = vertices[i] * (Terrain(vertices[i]) + 1);
             }
 
 
 
 
-        }
-        for (int i = 0; i < vertices.Count; i++)
-        {
-            vertices[i] = vertices[i].normalized;
-        }
-        for (int i = 0; i < vertices.Count; i++)
-        {
-            vertices[i] = vertices[i] * (Terrain(vertices[i]) + 1);
-        }
 
 
 
 
 
 
-
-
-
-
-        for (int h = 0; h < resolution; h++)
-        {
-            for (int i = 0; i < resolution; i++)
+            for (int h = 0; h < resolution; h++)
             {
-                triangles.Add((i) + h * (resolution + 1));
-                triangles.Add((i+resolution+1+1) + h * (resolution + 1));
-                triangles.Add((i+1) + h * (resolution + 1));
-                triangles.Add((i) + h * (resolution + 1));
-                triangles.Add((i + resolution + 1) + h * (resolution + 1));
-                triangles.Add((i + resolution + 1 + 1) + h * (resolution + 1));
+                for (int i = 0; i < resolution; i++)
+                {
+                    triangles.Add((i) + h * (resolution + 1));
+                    triangles.Add((i + resolution + 1 + 1) + h * (resolution + 1));
+                    triangles.Add((i + 1) + h * (resolution + 1));
+                    triangles.Add((i) + h * (resolution + 1));
+                    triangles.Add((i + resolution + 1) + h * (resolution + 1));
+                    triangles.Add((i + resolution + 1 + 1) + h * (resolution + 1));
+                }
+
             }
 
+
+
+
+
+
+
+
+
+            /*for (int c = 0; c < vertices.Count; c++)                                                                          //Test whether all vertices are added correctly (lowers performance drastically)
+            {
+                GameObject ball = Object.Instantiate(prefab) as GameObject;
+                ball.transform.position = vertices[c];
+               // cube.transform.parent = Planet.transform;
+            }*/
+            //Debug.Log(vertices.Count / 3);
+
+
+
+
+
+
+
+
+            mesh.Clear();
+            Vector3[] verticesarray = new Vector3[vertices.Count];
+            verticesarray = vertices.ToArray();
+            mesh.vertices = verticesarray;
+            int[] trianglearray = triangles.ToArray();
+            mesh.triangles = trianglearray;
+            Gen.GetComponent<MeshRenderer>().material = medal;
+            Gen.transform.position = new Vector3(-0.5f, -0.5f, -0.5f);
+            mesh.RecalculateNormals();
         }
+            
 
-
-
-
-
-
-
-
-
-         /*for (int c = 0; c < vertices.Count; c++)                                                                          //Test whether all vertices are added correctly (lowers performance drastically)
-         {
-             GameObject ball = Object.Instantiate(prefab) as GameObject;
-             ball.transform.position = vertices[c];
-            // cube.transform.parent = Planet.transform;
-         }*/
-        Debug.Log(vertices.Count / 3);
-
-
-
-
-
-
-
-
-
-
-
-        Vector3[] verticesarray = new Vector3[vertices.Count];
-        verticesarray = vertices.ToArray();
-        mesh.vertices = verticesarray;
-        int[] trianglearray = triangles.ToArray();
-        mesh.triangles = trianglearray;
-        Planet.GetComponent<MeshRenderer>().material = medal;
-        Planet.GetComponent<MeshFilter>().mesh = mesh;
-        Gen.transform.position =  new Vector3(-0.5f, -0.5f, -0.5f);
-        mesh.RecalculateNormals();
-
-
-
-
-
-
-
-
-            for (int i = 0; i < 6; i++)
+        for (int i = 0; i < 6; i++)
         {
-          GameObject meshObj = new GameObject("mesh" + i);
-          meshObj.transform.parent = transform;
-          meshObj.AddComponent<MeshRenderer>().sharedMaterial = medal;
-          meshObj.AddComponent<MeshFilter>();
-          meshObj.GetComponent<MeshFilter>().mesh = mesh;
-        
+            if (Gen.transform.childCount < 6)
+            {
+                meshObj = new GameObject();
+                meshObj = new GameObject("mesh" + i);
+                meshObj.transform.parent = Gen.transform;
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = medal;
+                meshObj.AddComponent<MeshFilter>();
+            }
         }
-        Gen.transform.GetChild(0).position = new Vector3(0, 0, 0);
-        Gen.transform.GetChild(0).Rotate(0, -180, 0);
-
-        Gen.transform.GetChild(1).position = new Vector3(0, 0, 0);
-        Gen.transform.GetChild(1).Rotate(0, -90, 0);
-
-        Gen.transform.GetChild(2).position = new Vector3(0, 0, 0);
-        Gen.transform.GetChild(2).Rotate(0, 90, 0);
-
-        Gen.transform.GetChild(3).position = new Vector3(0, 0, 0);
-        Gen.transform.GetChild(3).Rotate(-90, 90, 0);
-
-        Gen.transform.GetChild(4).position = new Vector3(0, 0, 0);
-        Gen.transform.GetChild(4).Rotate(90, 90, 0);
+        for (int i = 0; i < 6; i++)
+        {
+            Gen.transform.GetChild(i).GetComponent<MeshFilter>().mesh = mesh;
+        }
 
 
 
+
+
+
+
+            if (doesexist == false)
+        {
+            Gen.transform.GetChild(0).position = new Vector3(0, 0, 0);
+            Gen.transform.GetChild(0).Rotate(0, -180, 0);
+
+            Gen.transform.GetChild(1).position = new Vector3(0, 0, 0);
+            Gen.transform.GetChild(1).Rotate(0, -90, 0);
+
+            Gen.transform.GetChild(2).position = new Vector3(0, 0, 0);
+            Gen.transform.GetChild(2).Rotate(0, 90, 0);
+
+            Gen.transform.GetChild(3).position = new Vector3(0, 0, 0);
+            Gen.transform.GetChild(3).Rotate(-90, 90, 0);
+
+            Gen.transform.GetChild(4).position = new Vector3(0, 0, 0);
+            Gen.transform.GetChild(4).Rotate(90, 90, 0);
+            doesexist = true;
+        }
 
 
 
