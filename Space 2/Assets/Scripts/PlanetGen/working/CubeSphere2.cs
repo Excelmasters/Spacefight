e97990a2c2fb4949a6a5fff33c1c3a567d0a1919
Florian Hilprecht <florian@hilprecht.de>
2019-01-26 19:38:11 +0100

    a

--- Space 2/Assets/Scripts/PlanetGen/working/CubeSphere2.cs
+++ Space 2/Assets/Scripts/PlanetGen/working/CubeSphere2.cs
@@ -6,15 +6,17 @@ using UnityEngine;
 public class CubeSphere2 : MonoBehaviour
 {
     public GameObject prefab;
+    public GameObject prefab2;
     public GameObject Gen;
     private GameObject meshObj;
     public float radius;
-    [Range(0, 500)]
+    [Range(0, 10)]
     public int resolution;
     private Vector3 center;
     Noise noise = new Noise();
     public Material medal;
     public int size;
+    public Vector3[] normal;
     public bool doesexist = false;
 
 
@@ -26,8 +28,8 @@ public class CubeSphere2 : MonoBehaviour
     [Range(0, 1)]
     public float mininum;
     
-    private float amplitude;
-    private float frequenzy;
+    public float amplitude;
+    public float frequenzy;
     [Range(0, 10)]
     public int numsurfaces = 1;
     public float minimum;
@@ -50,27 +52,21 @@ public class CubeSphere2 : MonoBehaviour
 
 
 
-    
 
 
 
 
-    public float Terrain(Vector3 point)
+
+    public Vector3 Terrain(Vector3 vertice)
     {
-        float noiseValue = 0;
-        float frequency = Baseroughness;
-        float amplitude = 1;
+        // center.x = Random.Range(-1f, 1f);
+        //center.z = Random.Range(-1f, 1f);
+        //center.y = Random.Range(-1f, 1f);
 
-        for (int i = 0; i < numsurfaces; i++)
-        {
-            float v = noise.Evaluate(point * frequency + center);
-            noiseValue += (v + 1) * .5f * amplitude;
-            frequency *= roughness;
-            amplitude *= persistence;
-        }
 
-      //  noiseValue = Mathf.Max(0, noiseValue - settings.minValue);
-        return noiseValue * strength;
+        float TerrainValue = (noise.Evaluate((vertice * Baseroughness)) * amplitude);
+        float elevation = TerrainValue;
+        return vertice * (TerrainValue + 1);
     }
 
 
@@ -85,13 +81,19 @@ public class CubeSphere2 : MonoBehaviour
 
 
 
-    public Mesh MakeMesh()
-    {
 
+    public void OnValidate()
+    {
+        normal = new Vector3[6];
+        normal[0] = new Vector3(1,0,0);
+        normal[1] = new Vector3(-1,0,0);
+        normal[2] = new Vector3(0, 1, 0);
+        normal[3] = new Vector3(0, -1, 0);
+        normal[4] = new Vector3(0, 0, 1);
+        normal[5] = new Vector3(0, 0, -1);
 
         Mesh mesh = new Mesh();
-        for (int f = 0; f < 6; f++)
-        {
+
 
             if (Gen.GetComponent<MeshFilter>() == null) { Gen.AddComponent<MeshFilter>(); }
             if (Gen.AddComponent<MeshRenderer>() == null) { Gen.AddComponent<MeshFilter>(); }
@@ -110,28 +112,46 @@ public class CubeSphere2 : MonoBehaviour
 
 
 
-            for (int i = 0; i < resolution + 1; i++)
+
+        for (int k = 0; k < 6; k++)
+        {
+
+
+            Vector3 AxisA = new Vector3(normal[k].y, normal[k].z, normal[k].x);
+            Vector3 AxisB = Vector3.Cross(normal[k] , AxisA);
+
+           /* GameObject ball2 = Object.Instantiate(prefab2) as GameObject;
+            ball2.transform.position = AxisA ;
+
+            GameObject ball3 = Object.Instantiate(prefab2) as GameObject;
+            ball3.transform.position = AxisB;*/
+
+
+            for (int x = 0; x < resolution; x++)
             {
-                for (int h = 0; h < resolution + 1; h++)
+                for (int y = 0; y < resolution; y++)
                 {
-                    Vector3 PointonCube = (new Vector3((float)h / resolution * size - 0.5f, (float)i / resolution * size - 0.5f, 0 - 0.5f));
-                    Vector3 PointOnUnitSphere = PointonCube.normalized;
-                    // vertices.Add(Terrain(PointOnUnitSphere));
-                    vertices.Add(PointOnUnitSphere);
-
-
+                    vertices.Add(AxisA * x/resolution * 2 + AxisB * y/resolution * 2);
                 }
 
-
-
-
-
-
-
-
-
-
             }
+            for(int x = 0; x < vertices.Count; x++)
+            {
+                GameObject ball4 = Object.Instantiate(prefab2) as GameObject;
+                ball4.transform.position = vertices[x];
+            }
+        }
+
+
+        
+
+
+                for (int i   = 0; i < 6; i++)
+        {
+            GameObject ball = Object.Instantiate(prefab) as GameObject;
+            ball.transform.position = normal[i];
+        }
+
             
 
 
@@ -142,19 +162,36 @@ public class CubeSphere2 : MonoBehaviour
 
 
 
-            for (int h = 0; h < resolution; h++)
-            {
-                for (int i = 0; i < resolution; i++)
-                {
-                    triangles.Add((i) + h * (resolution + 1));
-                    triangles.Add((i + resolution + 1 + 1) + h * (resolution + 1));
-                    triangles.Add((i + 1) + h * (resolution + 1));
-                    triangles.Add((i) + h * (resolution + 1));
-                    triangles.Add((i + resolution + 1) + h * (resolution + 1));
-                    triangles.Add((i + resolution + 1 + 1) + h * (resolution + 1));
-                }
 
-            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /* for (int h = 0; h < resolution; h++)
+             {
+                 for (int i = 0; i < resolution; i++)
+                 {
+                     triangles.Add((i) + h * (resolution + 1));
+                     triangles.Add((i + resolution + 1 + 1) + h * (resolution + 1));
+                     triangles.Add((i + 1) + h * (resolution + 1));
+                     triangles.Add((i) + h * (resolution + 1));
+                     triangles.Add((i + resolution + 1) + h * (resolution + 1));
+                     triangles.Add((i + resolution + 1 + 1) + h * (resolution + 1));
+                 }
+
+             }*/
 
 
 
@@ -188,48 +225,9 @@ public class CubeSphere2 : MonoBehaviour
             Gen.GetComponent<MeshRenderer>().material = medal;
             mesh.RecalculateNormals();
 
-
-
-
-
-        }
-        return mesh;
-    }
-    public void OnValidate()
-    {
-        for (int i = 0; i < 6; i++)
-        {
-            if (Gen.transform.childCount < 6)
-            {
-                meshObj = new GameObject("mesh" + (i + 1));
-                meshObj.transform.parent = Gen.transform;
-                meshObj.AddComponent<MeshRenderer>().sharedMaterial = medal;
-                meshObj.AddComponent<MeshFilter>();
-            }
-        }
-        for (int i = 0; i < 6; i++)
-        {
-
-
-            /* for (int gh = 0; gh < mesh.vertices.Length; gh++)
-             {
-                 mesh.vertices[gh] = mesh.vertices[gh] * (Terrain(mesh.vertices[gh]) + 1);
-             }*/
-            Mesh mesh = MakeMesh();
-            Vector3[] vertices = mesh.vertices;
-            center.x = Random.Range(0f, 5f);
-            center.y = Random.Range(0f, 5f);
-            center.z = Random.Range(0f, 5f);
-            for (int u = 0; u < vertices.Length; u++)
-            {
-                vertices[u] = Terrain(mesh.vertices[u]) * mesh.vertices[u];
-                //vertices[u] = vertices[u] * Random.Range(1.5f, 0.5f);
-
-            }
-            mesh.vertices = vertices;
-
-            Gen.transform.GetChild(i).GetComponent<MeshFilter>().mesh = mesh;
-        }
+            Gen.GetComponent<MeshFilter>().mesh = mesh;
+            
+        
 
 
 
@@ -238,7 +236,8 @@ public class CubeSphere2 : MonoBehaviour
 
 
 
-        if (doesexist == false)
+
+       /* if (doesexist == false)
         {
             Gen.transform.GetChild(0).position = new Vector3(0, 0, 0);
             Gen.transform.GetChild(0).Rotate(0, -180, 0);
@@ -255,7 +254,7 @@ public class CubeSphere2 : MonoBehaviour
             Gen.transform.GetChild(4).position = new Vector3(0, 0, 0);
             Gen.transform.GetChild(4).Rotate(90, 90, 0);
             doesexist = true;
-        }
+        }*/
 
     }
 
