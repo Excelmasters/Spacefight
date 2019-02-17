6c124afedceeacd7a909cc0933e908d22f7e73d2
Florian Hilprecht <florian@hilprecht.de>
2019-02-17 17:56:10 +0100

    s

--- Space 2/Assets/SunColor.cs
+++ Space 2/Assets/SunColor.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.VFX;
 
 public class SunColor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool start;
+    public VisualEffect sun;
+    public  Gradient gradient;
+
+    void OnValidate()
     {
-        
+        GradientColorKey[] colorKey = new GradientColorKey[(int)Random.Range(3, 7)];
+        for (int i = 0; i < colorKey.Length; i++)
+        {
+            colorKey[i].time = Random.Range(0f, 1f);
+            colorKey[i].color = new Color(Random.Range(0, 255), Random.Range(0, 255), Random.Range(0, 255)) / 250;
+        }
+        GradientAlphaKey[] alphaKey = new GradientAlphaKey[0];
+        gradient.SetKeys(colorKey, alphaKey);
+
+        sun = GetComponent<VisualEffect>();
+        sun.SetGradient(0, gradient);
+
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
