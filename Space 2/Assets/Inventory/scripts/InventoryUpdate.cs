78eab2cecef581bf8b7f4107b978e1a87ac0a027
Iceshot <florian.bartusek@gmail.com>
2019-03-10 00:41:15 +0100

    updates

---
+++ Space 2/Assets/Inventory/scripts/InventoryUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUpdate : MonoBehaviour
+{
+    public int count = 19;
+    public Item Blöcke;
+    private InventoryHolder holder;
+
+    private void Start()
+    {
+        holder = GetComponent<InventoryHolder>();
+    }
+    void Update()
+    {
+            holder.Clearblock();
+            ItemStack block = new ItemStack(Blöcke, count);
+            holder.AddStack(block);
+    }
+}
---
+++ Space 2/Assets/Scripts/Shipstuff/Laser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public GameObject prefab;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject projectile = GameObject.Instantiate(prefab) as GameObject;
+        }
+    }
+}
